e2a0163c117eb2902a2c0f5919ddba61dac28288
Daria Kliukhina <d.kliukhina@pink-man.ru>
2020-04-27 23:04:48 +0300

    Исправляет чтение из файлов на promise.all

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -25,7 +25,7 @@ const readContent = async (filePath) => {
     return content.split(`\n`);
   } catch (err) {
     console.error(chalk.red(err));
-    return [];
+    throw err;
   }
 };
 
@@ -60,33 +60,37 @@ const generateOffers = (count, titles, categories, sentences) => {
   return offersArray;
 };
 
-const writeToFile = (content) => {
-  fs.writeFile(FILE_NAME, content);
-};
 
 module.exports = {
   name: `--generate`,
   async run(args) {
-    const sentences = await readContent(FILE_SENTENCES_PATH);
-    const titles = await readContent(FILE_TITLES_PATH);
-    const categories = await readContent(FILE_CATEGORIES_PATH);
-
-    const [count] = args;
-    const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
-
-    if (countOffer > MAX_COUNT) {
-      console.log(chalk.red(`Ошибка. Максимальное число файлов ${MAX_COUNT}`));
-      process.exit(ExitCode.error);
-    }
-
-    const offers = generateOffers(countOffer, titles, categories, sentences);
-    const content = JSON.stringify(offers);
     try {
-      await writeToFile(content);
-      console.info(chalk.green(`Operation success. File created.`));
+      const [count] = args;
+      const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
+
+      if (countOffer > MAX_COUNT) {
+        console.log(chalk.red(`Ошибка. Максимальное число файлов ${MAX_COUNT}`));
+        process.exit(ExitCode.error);
+      }
+
+      const readSentences = readContent(FILE_SENTENCES_PATH);
+      const readTitles = readContent(FILE_TITLES_PATH);
+      const readCategories = readContent(FILE_CATEGORIES_PATH);
+
+      const [sentences, titles, categories] = await Promise
+        .all([
+          readSentences,
+          readTitles,
+          readCategories
+        ]);
+
+      const offers = generateOffers(countOffer, titles, categories, sentences);
+      const content = JSON.stringify(offers);
+
+      await fs.writeFile(FILE_NAME, content);
+      console.log(chalk.green(`Operation success. File created.`));
     } catch (err) {
-      console.error(chalk.red(`Can't write data to file...`));
-      process.exit(ExitCode.error);
+      console.error(chalk.red(`Can't write data to file. ${err}`));
     }
   }
 };
