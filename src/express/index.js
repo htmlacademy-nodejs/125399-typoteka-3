b246423e2961d08fed6a99392f572664619d052f
Daria Kliukhina <d.kliukhina@pink-man.ru>
2020-05-17 18:32:01 +0300

    Добавляет шаблоны

--- src/express/index.js
+++ src/express/index.js
@@ -1,14 +1,21 @@
 'use strict';
 
 const express = require(`express`);
-
-const articlesRoutes = require(`./routes/articles-routes`);
-const myRoutes = require(`./routes/my-routes`);
-const mainRoutes = require(`./routes/main-routes`);
+const path = require(`path`);
 
 const DEFAULT_PORT = 8080;
 
 const app = express();
+
+const PUBLIC_DIR = `public`;
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
+
+const articlesRoutes = require(`./routes/articles.routes`);
+const myRoutes = require(`./routes/my.routes`);
+const mainRoutes = require(`./routes/main.routes`);
 
 app.use(`/articles`, articlesRoutes);
 app.use(`/my`, myRoutes);
---
+++ src/express/routes/main.routes.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const {Router} = require(`express`);
+const mainRouter = new Router();
+
+mainRouter.get(`/`, (req, res) => res.render(`main`));
+mainRouter.get(`/register`, (req, res) => res.render(`login`));
+mainRouter.get(`/login`, (req, res) => res.render(`login`));
+mainRouter.get(`/search`, (req, res) => res.render(`search`));
+mainRouter.get(`/categories`, (req, res) => res.render(`all-categories`));
+
+module.exports = mainRouter;
