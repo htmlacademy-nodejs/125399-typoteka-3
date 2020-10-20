e06ba9e732159ac8118491a56de6a0881f73e390
Daria Kliukhina <d.kliukhina@pink-man.ru>
2020-10-20 10:40:11 +0300

    Добавляет запрос axios

--- src/express/api.js
+++ src/express/api.js
@@ -3,7 +3,7 @@
 const axios = require(`axios`);
 const TIMEOUT = 1000;
 const port = process.env.API_PORT || 3000;
-const defaultUrl = `http://localhost:${port}/api/`;
+const defaultUrl = `http://localhost:${port}/api`;
 
 
 class API {
@@ -15,16 +15,21 @@ class API {
   }
 
   async _load(url, options) {
-    const response = await this._http.request({url, ...options});
-    return response.data;
+    let response;
+    try {
+      response = await this._http.request({url, ...options});
+    } catch (err) {
+      console.error(err);
+    }
+    return response;
   }
 
-  getOffers() {
-    return this._load(`/offers`);
+  getArticles() {
+    return this._load(`/articles`);
   }
 
   getOffer(id) {
-    return this._load(`/offers/${id}`);
+    return this._load(`/articles/${id}`);
   }
 
   search(query) {
@@ -36,7 +41,7 @@ class API {
   }
 
   async createOffer(data) {
-    return this._load(`/offers`, {
+    return this._load(`/articles`, {
       method: `POST`,
       data
     });
